97405d6613a530a43a2305a2e313bd58979ffe4f
Toby Goodwin <toby@paccrat.org>
2016-04-16 11:55:09 +0100

    creates tables, writes version

--- error.h
+++ error.h
@@ -1,14 +1,16 @@
 void fatal1(const char *);
+void fatal2(const char *, const char *);
 void fatal3(const char *, const char *, const char *);
-void fatal1x(const char *);
-void fatal3x(const char *, const char *, const char *);
-void fatal5x(const char *, const char *, const char *,
-        const char *, const char *);
+void fatal4(const char *, const char *, const char *, const char *);
 void fatal5(const char *, const char *, const char *,
 	const char *, const char *);
 void fatal9(const char *, const char *, const char *,
 	const char *, const char *, const char *,
         const char *, const char *, const char *);
+void fatal1x(const char *);
+void fatal3x(const char *, const char *, const char *);
+void fatal5x(const char *, const char *, const char *,
+        const char *, const char *);
 
 void nomem(void);
 
