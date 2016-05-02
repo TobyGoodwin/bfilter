ff86780d096de0388fed95d6625692dcc3eda654
Toby Goodwin <toby@paccrat.org>
2016-05-02 22:26:25 +0100

    no unit/count tests at present

--- test/unit/count.c
+++ test/unit/count.c
@@ -1,10 +1,16 @@
 /*
+true
+*/
+
+/*
 depends test/ucount
 export BFILTER_DB=$(mktemp -u)
 mcheck '3,1,' 3
 mcheck '3,2,' 3
 mcheck '3,2,5,2,' 5 5
 */
+
+// XXX we could definitely do with some tests of db_count_update
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +25,7 @@ int main(int argc, char **argv) {
     if (!db_open()) return 1;
     for (i = 1; i < argc; ++i) {
         int c = atoi(argv[i]);
-        count_add((uint8_t *)"bar", 3, c, 1);
+        count_update(1, 3, c, 1);
     }
     r = db_get_intlist((uint8_t *)"bar", 3, &n);
     for (i = 0; i < n; ++i)
